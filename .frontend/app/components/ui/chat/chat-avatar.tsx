f052f77f2c9043f6086503c2b1ad59f8af924e4a
Alberto <alberto.arm@gmail.com>
2024-12-06 09:02:20 +0000

    Frontend Bolueta

--- .frontend/app/components/ui/chat/chat-avatar.tsx
+++ .frontend/app/components/ui/chat/chat-avatar.tsx
@@ -1,5 +1,5 @@
 import { useChatMessage } from "@llamaindex/chat-ui";
-import { User2 } from "lucide-react";
+import { UserSearch } from "lucide-react";
 import Image from "next/image";
 
 export function ChatMessageAvatar() {
@@ -7,7 +7,7 @@ export function ChatMessageAvatar() {
   if (message.role === "user") {
     return (
       <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-background shadow">
-        <User2 className="h-4 w-4" />
+        <UserSearch className="h-4 w-4" />
       </div>
     );
   }
@@ -16,8 +16,8 @@ export function ChatMessageAvatar() {
     <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-black text-white shadow">
       <Image
         className="rounded-md"
-        src="/llama.png"
-        alt="Llama Logo"
+        src="/bolueta.png"
+        alt="Bolueta Logo"
         width={24}
         height={24}
         priority
